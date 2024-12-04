6b5ac1fd2b7bfa4d1a2641c7d77fe9f4b38d1e10
Tahmidrk <23524202041@student.bup.edu.bd>
2024-12-04 22:52:47 +0600

    Add files via upload

---
+++ multiplicationtwomatrix.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r,c;
+    cout<<"Enter row & column:"<<endl;
+    cin>>r>>c;
+    int a[r][c],b[r][c],mul[r][c];
+    
+    
+
+    cout<<"Enter array 1 elements:"<<endl;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>a[i][j];
+        }
+    }
+  
+      cout<<"Enter array 2 elements:"<<endl;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>b[i][j];
+        }
+    }
+
+     for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            mul[i][j]=a[i][j]*b[i][j];
+        }
+        
+    }
+    cout<<"Sum matrix:"<<endl;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cout<<mul[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
